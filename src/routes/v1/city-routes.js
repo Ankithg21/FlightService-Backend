bf6f6b2ac0868ecf8e400c45f9f6fb1a20a07e47
Ankith G <ankithg2104@gmail.com>
2025-06-09 11:19:07 +0530

    Added Middleware for Create City API-name of city Validation

--- src/routes/v1/city-routes.js
+++ src/routes/v1/city-routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const {CityController} = require("../../controllers");
+const {CityMiddlewares} = require("../../middlewares");
 
 // POST /api/v1/cities
 router.post(
     "/",
+    CityMiddlewares.validateCreateRequest,
     CityController.createCity
 );
 
