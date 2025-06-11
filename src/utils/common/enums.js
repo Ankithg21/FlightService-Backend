1ab3fb932fafee19230d402401e24a676302aabd
Ankith G <ankithg2104@gmail.com>
2025-06-11 18:43:00 +0530

    Added Seats Model schema, with Associations

---
+++ src/utils/common/enums.js
@@ -0,0 +1,10 @@
+const SEAT_TYPE = {
+    BUSSINESS: "Bussiness",
+    ECONOMY: "Economy",
+    PREMIUM_ECONOMY: "Premium-economy",
+    FIRST_CLASS: "First-class"
+}
+
+module.exports = {
+    SEAT_TYPE,
+}
