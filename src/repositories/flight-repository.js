4dbf3e9ce58072a69743a44b939b6742838ed25c
Ankith G <ankithg2104@gmail.com>
2025-06-15 10:09:44 +0530

    Fixed a bug in updateRemainingSeat API

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -51,7 +51,7 @@ class FlightRepository extends CrudRepository{
     async updateRemainingSeats(flightId, seats, dec=1){
         await db.sequelize.query(addRowLockOnFlights(flightId));
         const flight = await Flight.findByPk(flightId);
-        if(parseInt(dec)){
+        if(+dec){
             await flight.decrement('totalSeats', {by: seats});
         }
         else{
